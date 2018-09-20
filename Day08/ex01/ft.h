42f6c296601380536b7cf6e688d33dd777b604f0
Youssef Idabdeslem <yidabdes@e1r8p10.1337.ma>
2018-09-20 22:32:34 +0100

    Added The Last Day

---
+++ Day08/ex01/ft.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft.h                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yidabdes <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/09/20 17:27:49 by yidabdes          #+#    #+#             */
+/*   Updated: 2018/09/20 17:27:54 by yidabdes         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_H
+# define FT_H
+
+void	ft_putchar(char c);
+void	ft_swap(int *a, int *b);
+void	ft_putstr(char *str);
+int		ft_strlen(char *str);
+int		ft_strcmp(char *s1, char *s2);
+
+#endif
