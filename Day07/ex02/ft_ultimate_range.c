52c4c7c4ca85407e039506bab36cca85b5aa8832
Youssef Idabdeslem <yidabdes@e1r8p10.1337.ma>
2018-09-20 20:30:09 +0100

    hello'

---
+++ Day07/ex02/ft_ultimate_range.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ultimate_range.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yidabdes <ussef.id@gmail.com>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/09/15 15:19:47 by yidabdes          #+#    #+#             */
+/*   Updated: 2018/09/15 15:19:50 by yidabdes         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int		ft_ultimate_range(int **range, int min, int max)
+{
+	int i;
+	int *tab;
+
+	if (min >= max)
+	{
+		*range = (void *)0;
+		return (0);
+	}
+	tab = (int*)malloc(sizeof(*tab) * (max - min));
+	i = 0;
+	while (min < max)
+	{
+		tab[i] = min;
+		i++;
+		min++;
+	}
+	*range = tab;
+	return (i);
+}
